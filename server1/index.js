c1e62ef7d2eb5c1177f023c5d12c8d4f14022287
Lai Phuc Hau <laiphuchau112004@gmail.com>
2025-07-01 13:26:47 +0700

    Initial commit: Server 1 and Server 2

---
+++ server1/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Đây là Server 1');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server 1 chạy ở cổng ${PORT}`);
+});
